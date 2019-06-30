bf6b50b6848e4aa7085a182c99b794179ef77343
Matteo Mendula <33020977+MatteoMendula@users.noreply.github.com>
2019-06-30 17:49:34 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = express()
   .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const wss = new SocketServer({ server });
+const wss = new SocketServer({ server },() => console.log("socket up"));
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
